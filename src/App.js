859cf6a3f6086ef25c5c07f770724e30b1202897
Roshan Shibi <roshanshibi2015@gmail.com>
2022-06-20 22:06:27 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -17,18 +17,41 @@ function App() {
       </div>
       <div className="input">
         <input value={toDo} onChange={(e)=>setTodo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
-        <i className="fas fa-plus"></i>
+        <i onClick={()=>setToDos([...toDos,{id:Date.now(),text:toDo,status:false}])} className="fas fa-plus"></i>
       </div>
       <div className="todos">
-        <div className="todo">
+
+{       toDos.map((obj)=>{
+
+        return (<div className="todo">
           <div className="left">
-            <input type="checkbox" name="" id="" />
-            <p>React tutorial</p>
+            <input onChange={(e)=>{
+            console.log(e.target.checked)
+            console.log(obj)
+
+            setToDos(toDos.filter(obj2=>{
+                if(obj.id===obj2.id){
+                  obj.status=e.target.checked
+                }
+                return obj2
+            }))}} value={obj.status} type="checkbox" name="" id="" />
+            <p>{obj.text}</p>
           </div>
           <div className="right">
             <i className="fas fa-times"></i>
           </div>
-        </div>
+        </div>)
+})}
+
+{
+  toDos.map((obj)=>{
+    if(obj.status){
+      return(<h1>{obj.text}</h1>)
+    }
+    return null
+  })
+}
+
       </div>
     </div>
   );
