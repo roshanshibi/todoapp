6509d445409719d45ff414e0a2773d1cfc9bcfc7
Roshan Shibi <roshanshibi2015@gmail.com>
2022-06-22 02:30:25 +0530

    added styling and categories

--- src/App.js
+++ src/App.js
@@ -2,58 +2,129 @@ import React from 'react';
 import './App.css';
 import {useState} from 'react';
 
+
 function App() {
 
   const [toDos,setToDos] = useState([]);
   const [toDo,setTodo] = useState([''])
+  const [delToDo,setDelToDo] = useState([])
+  // const current = new Date();
+  // const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
   return (
+    
     <div className="app">
+    
+<div class="">
+    <div class="w3-panel w3-card-4 w3-grey">
       <div className="mainHeading">
         <h1>ToDo List</h1>
+        
       </div>
       <div className="subHeading">
         <br />
        
       </div>
+      
       <div className="input">
         <input value={toDo} onChange={(e)=>setTodo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
         <i onClick={()=>setToDos([...toDos,{id:Date.now(),text:toDo,status:false}])} className="fas fa-plus"></i>
+      </div><br /><br />
       </div>
-      <div className="todos">
+      
 
-{       toDos.map((obj)=>{
+      <div class="">
+    <div class="w3-panel w3-card-4 w3-grey">
+      <div className="todos">
+        <h1>Ongoing</h1>
+
+        { toDos.map((obj,key)=>{
 
         return (<div className="todo">
           <div className="left">
             <input onChange={(e)=>{
-            console.log(e.target.checked)
-            console.log(obj)
-
             setToDos(toDos.filter(obj2=>{
                 if(obj.id===obj2.id){
                   obj.status=e.target.checked
                 }
                 return obj2
-            }))}} value={obj.status} type="checkbox" name="" id="" />
-            <p>{obj.text}</p>
-          </div>
+            }))}} value={obj.status} class="strikethrough" type="checkbox" name="" id="" />
+            <span><p>{obj.text}</p></span>
+          </div><br/>
           <div className="right">
-            <i className="fas fa-times"></i>
+            <i onClick={() => {
+                  if (window.confirm("Are you sure ?")) {
+                    setDelToDo((delToDo) => [...delToDo, obj]);
+                    setToDos(toDos.filter(obj2 => {
+                      if (obj2.id !== obj.id) {
+                        return obj2;
+                      }
+                      return null;
+                    }))
+                  }
+                }} className="fas fa-times"></i>
           </div>
+          
         </div>)
 })}
+        </div>
+      </div>
+    </div>
+  </div>
 
-{
+
+
+
+{/* Completed container_________________________________________________________ */}
+      <div class="">
+    <div class="w3-panel w3-card-4 w3-grey">
+    <div className="todos">
+      <h1 align="center">Completed</h1>
+      {
   toDos.map((obj)=>{
     if(obj.status){
-      return(<h1>{obj.text}</h1>)
+      return(<div className="todo"><h3>{obj.text}</h3><i onClick={() => {
+        if (window.confirm("Are you sure ?")) {
+          setDelToDo((delToDo) => [...delToDo, obj]);
+          setToDos(toDos.filter(obj2 => {
+            if (obj2.id !== obj.id) {
+              return obj2;
+            }
+            return null;
+          }))
+        }
+      }} className="fas fa-times"></i></div>)
     }
     return null
   })
 }
-
       </div>
+      </div>
+      </div>
+{/* ________________________________________________________________________ */}
+
+
+
+{/* Deleted container_________________________________________________________ */}
+      <div class="">
+    <div class="w3-panel w3-card-4 w3-grey">
+    <div className="todos">
+      <h1 align="center">Deleted</h1>
+      {
+  delToDo.map((obj)=>{
+    if(obj){
+      return(<div className="todo"><h3>{obj.text}</h3></div>)
+    }
+    return null
+  })
+}
+      </div>
+      </div>
+      </div>
+{/* ________________________________________________________________________ */}
+
+
     </div>
+   
   );
 }
 
